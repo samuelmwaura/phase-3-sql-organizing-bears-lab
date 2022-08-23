007a113d6e744021313333fa43dedc0c489e4778
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-08-23 14:31:42 +0300

    commit tree 3b179bdf165b188dcf73b365aa959005e01e752f

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+insert into bears (name,age,sex,color,temperament,alive) values 
+("Mr. Chocolate", 45,"M","white","high",0),
+("Rowdy", 45,"F","mullato","low",1),
+("Tabitha", 45,"M","black","high",1),
+("Sergeant Brown", 45,"F","white","low",0),
+("Melissa", 45,"F","mullato","high",1),
+("Grinch", 45,"M","black","high",0),
+("Wendy", 45,"F","black","low",1),
+(null, null,"F","black","high",0);
